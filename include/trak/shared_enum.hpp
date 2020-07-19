938f5bbe53610bf20ed507957cc92c654ebc4a7a
Anders Løvig <anders.lloevig@gmail.com>
2020-07-19 10:38:57 +0200

    Initial commit

---
+++ include/trak/shared_enum.hpp
@@ -0,0 +1,153 @@
+#ifndef TRAK_SHARED_ENUM_HPP
+#define TRAK_SHARED_ENUM_HPP
+
+#include <type_traits>
+
+namespace trak {
+
+    /**
+     * Represents an enum value being member of multiple enums.
+     * The value is implicitly convertible to each member type.
+     *
+     * @tparam Ts
+     *      List of types this enum value is member of.
+     */
+    template<typename... Ts>
+    class shared_enum;
+
+    /**
+     * Checks whether U is a member of a shared enum. Provides the member constant value which is
+     * equal to \c true if U is in the list of types Ts. Otherwise, value is \c false.
+     *
+     * This declaration breaks the recursive definition is_member_of_shared_enum::value.
+     *
+     * @tparam U
+     *      The type to test for membership.
+     * @tparam Ts
+     *      The tail of the list of shared enum types.
+     */
+    template<typename U, typename... Ts>
+    struct is_member_of_shared_enum {
+        static const bool value = false;
+    };
+
+    /**
+     * Checks whether U is a member of a shared enum. Provides the member constant value which is
+     * equal to \c true if U is the same as T or in the list of types Ts. Otherwise, value is \c false.
+     *
+     * The membership test is done using a recursive definition of value on the list tail Ts.
+     *
+     * @tparam U
+     *      The type to test for membership.
+     * @tparam T
+     *      The head of the list of shared enum types.
+     * @tparam Ts
+     *      The tail of the list of shared enum types.
+     */
+    template<typename U, typename T, typename... Ts>
+    struct is_member_of_shared_enum<U, T, Ts...> {
+        static const bool value = std::conditional<std::is_same<U, T>::value, std::true_type, is_member_of_shared_enum<U, Ts...>>::type::value;
+    };
+
+    /**
+     * Base of shared enums containing the actual enum value.
+     *
+     * @tparam T
+     *      The underlying type of the value.
+     */
+    template<typename T>
+    class shared_enum_base {
+    public:
+        /**
+         * @param value
+         *      The value of the shared enum.
+         */
+        constexpr inline explicit shared_enum_base(T value) noexcept : value_(value) {}
+
+        /**
+         * Cast operator to the underlying type.
+         *
+         * @return
+         *      The enum value.
+         */
+        constexpr inline explicit operator T() const noexcept {
+            return value_;
+        };
+
+    protected:
+        T value_;
+    };
+
+    /**
+     * Invalid shared_enum template instantiation.
+     */
+    template<>
+    class shared_enum<> {};
+
+    /**
+     * Represents an enum value being a member of multiple enums.
+     * The value is implicitly convertible to T.
+     *
+     * @tparam T
+     *      The valid enum type of this shared enum.
+     */
+    template<typename T>
+    class shared_enum<T> : public shared_enum_base<typename std::underlying_type<T>::type> {
+    public:
+        using underlying_type = typename std::underlying_type<T>::type;
+
+        /**
+         * Constructor
+         *
+         * @tparam U
+         *      The parameter type of value which is implicitly convertible to the underlying type of T.
+         * @param value
+         *      The value of this shared enum.
+         */
+        template<typename U>
+        constexpr inline shared_enum(U value) noexcept
+                : shared_enum_base<underlying_type>(static_cast<underlying_type>(value)) {
+            static_assert(is_member_of_shared_enum<U, T>::value, "U is not a member of shared enum");
+        }
+
+        /**
+         * Constructor
+         *
+         * @param value
+         *      The value of this shared enum.
+         */
+        constexpr inline explicit shared_enum(underlying_type value) noexcept
+                : shared_enum_base<underlying_type>(value) {}
+
+        /**
+         * Cast operator allowing for implicit conversion to T.
+         *
+         * @return
+         *      The enum value as T.
+         */
+        constexpr inline operator T() const noexcept {
+            return static_cast<T>(this->value_);
+        }
+    };
+
+    template<typename T, typename... Ts>
+    class shared_enum<T, Ts...> : shared_enum<Ts...> {
+    public:
+        using underlying_type = typename shared_enum<Ts...>::underlying_type;
+
+        /**
+         * Constructor
+         *
+         * @tparam U
+         *      The parameter type of value which is implicitly convertible to the underlying type of T.
+         * @param value
+         *      The value of this shared enum.
+         */
+        template<typename U>
+        constexpr inline shared_enum(U value) noexcept : shared_enum<Ts...>(static_cast<underlying_type>(value)) {
+            static_assert(is_member_of_shared_enum<U, T, Ts...>::value, "U is not a member of shared enum");
+        }
+    };
+}
+
+#endif //TRAK_SHARED_ENUM_HPP
---
+++ test/shared_enum_test.cpp
@@ -0,0 +1,63 @@
+#include <gtest/gtest.h>
+#include <trak/shared_enum.hpp>
+
+using namespace trak;
+
+enum class A : unsigned int {
+    Aa = 1,
+    Ab = 2,
+    Ac = 3
+};
+
+enum class B : unsigned int {
+    Ba,
+    Bb,
+    Bc
+};
+
+enum class C : unsigned int {
+    Ca,
+    Cb,
+    Cc
+};
+
+unsigned int takes_A(A value) {
+    return static_cast<unsigned int>(value);
+}
+
+unsigned int takes_B(B value) {
+    return static_cast<unsigned int>(value);
+}
+
+unsigned int takes_C(C value) {
+    return static_cast<unsigned int>(value);
+}
+
+TEST(shared_enum, member) {
+    auto test = is_member_of_shared_enum<A, A, B, C>::value;
+    EXPECT_TRUE(test);
+    test = is_member_of_shared_enum<B, A, B, C>::value;
+    EXPECT_TRUE(test);
+    test = is_member_of_shared_enum<C, A, B, C>::value;
+    EXPECT_TRUE(test);
+
+    test = is_member_of_shared_enum<A>::value;
+    EXPECT_FALSE(test); // A is not in the empty shared enum
+    test = is_member_of_shared_enum<A, B>::value;
+    EXPECT_FALSE(test); // A is not B
+    test = is_member_of_shared_enum<C, A, B>::value;
+    EXPECT_FALSE(test); // C is neither A or B
+}
+
+TEST(shared_enum, intersection) {
+
+}
+
+//TEST(shared_enum, convertible) {
+//    shared_enum<A, B, C> value = A::Aa;
+//
+//    EXPECT_EQ(1, takes_A(value));
+//    EXPECT_EQ(1, takes_B(value));
+//    EXPECT_EQ(1, takes_C(value));
+//}
+
